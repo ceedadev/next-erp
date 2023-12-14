d16b74d154fa4eae5c1c8c0fe02fa24c676e80e9
ceedadev <ceedadev@gmail.com>
2023-12-14 17:29:36 +0700

    Refactor product form component and add validation for product fields

--- src/components/forms/product-form.tsx
+++ src/components/forms/product-form.tsx
@@ -1,9 +1,9 @@
 "use client";
+import * as React from "react";
 
-import { CreateProduct } from "@/lib/validations/product";
-import { zodResolver } from "@hookform/resolvers/zod";
 import * as z from "zod";
 import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
 
 import {
   Form,
@@ -15,17 +15,47 @@ import {
   FormMessage,
 } from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+
+import { sluggify } from "@/lib/format";
+import { CreateProduct } from "@/lib/validations/product";
 
 export default function ProductForm() {
+  // File input
+  const [selectedImage, setSelectedImage] = React.useState<string | null>(null);
+  const handleImageChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const file = e.target.files?.[0];
+    if (file) {
+      const reader = new FileReader();
+      reader.onloadend = () => {
+        setSelectedImage(reader.result as string);
+      };
+      reader.readAsDataURL(file);
+      form.setValue("image", "IMAGE URL FROM STORAGE");
+    }
+  };
+
   const form = useForm<z.infer<typeof CreateProduct>>({
     resolver: zodResolver(CreateProduct),
     defaultValues: {
       name: "",
+      price: "",
+      sku: "",
+      description: "",
+      image: null,
     },
   });
   function onSubmit(values: z.infer<typeof CreateProduct>) {
+    if (values.name) {
+      values.slug = sluggify(values.name);
+    }
     console.log(values);
   }
+  function onClear() {
+    form.reset();
+    setSelectedImage(null);
+  }
+
   return (
     <Form {...form}>
       <form onSubmit={form.handleSubmit(onSubmit)}>
@@ -33,21 +63,23 @@ export default function ProductForm() {
           control={form.control}
           name="name"
           render={({ field }) => (
-            <FormItem className="flex flex-col md:flex-row gap-2 p-4 items-center">
+            <FormItem className="flex flex-col md:flex-row gap-2 p-4 items-start">
               <div className="flex flex-col gap-2 min-w-[300px]">
                 <FormLabel>Product Name</FormLabel>
                 <FormDescription>
                   Product name must be unique and cannot be changed later.
                 </FormDescription>
               </div>
-              <FormControl>
-                <Input
-                  placeholder="Product Name"
-                  {...field}
-                  autoComplete="off"
-                />
-              </FormControl>
-              <FormMessage />
+              <div className="flex flex-col w-full space-y-2">
+                <FormControl>
+                  <Input
+                    placeholder="Product Name"
+                    {...field}
+                    autoComplete="off"
+                  />
+                </FormControl>
+                <FormMessage />
+              </div>
             </FormItem>
           )}
         />
@@ -55,23 +87,122 @@ export default function ProductForm() {
           control={form.control}
           name="price"
           render={({ field }) => (
-            <FormItem className="flex flex-col md:flex-row gap-2 p-4 items-center">
+            <FormItem className="flex flex-col md:flex-row gap-2 p-4 items-start">
               <div className="flex flex-col gap-2 min-w-[300px]">
                 <FormLabel>Product Price</FormLabel>
-                <FormDescription>Selling price per item</FormDescription>
+                <FormDescription>Selling price per item.</FormDescription>
               </div>
-              <FormControl>
-                <Input
-                  type="number"
-                  placeholder="Product Price"
-                  {...field}
-                  autoComplete="off"
-                />
-              </FormControl>
-              <FormMessage />
+              <div className="flex flex-col w-full space-y-2">
+                <FormControl>
+                  <Input
+                    type="number"
+                    placeholder="Product Price"
+                    {...field}
+                    autoComplete="off"
+                  />
+                </FormControl>
+                <FormMessage />
+              </div>
             </FormItem>
           )}
         />
+        <FormField
+          control={form.control}
+          name="sku"
+          render={({ field }) => (
+            <FormItem className="flex flex-col md:flex-row gap-2 p-4 items-start">
+              <div className="flex flex-col gap-2 min-w-[300px]">
+                <FormLabel>Product SKU</FormLabel>
+                <FormDescription>
+                  Stock Keeping Unit, must be unique.
+                </FormDescription>
+              </div>
+              <div className="flex flex-col w-full space-y-2">
+                <FormControl>
+                  <Input
+                    type="text"
+                    placeholder="Product SKU"
+                    autoComplete="off"
+                    {...field}
+                  />
+                </FormControl>
+                <FormMessage />
+              </div>
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name="description"
+          render={({ field }) => (
+            <FormItem className="flex flex-col md:flex-row gap-2 p-4 items-start">
+              <div className="flex flex-col gap-2 min-w-[300px]">
+                <FormLabel>Product Description</FormLabel>
+                <FormDescription>
+                  Product description, can be changed later.
+                </FormDescription>
+              </div>
+              <div className="flex flex-col w-full space-y-2">
+                <FormControl>
+                  <textarea
+                    className="w-full p-2 border rounded-md bg-background focus:ring-1 focus:ring-primary focus:outline-none text-base placeholder:text-muted-foreground placeholder:text-sm min-h-[100px]"
+                    placeholder="Product Description"
+                    {...field}
+                  />
+                </FormControl>
+                <FormMessage />
+              </div>
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name="image"
+          render={({ field }) => (
+            <FormItem className="flex flex-col md:flex-row gap-2 p-4 items-start">
+              <div className="flex flex-col gap-2 min-w-[300px]">
+                <FormLabel>Product Image</FormLabel>
+                <FormDescription>
+                  Stock Keeping Unit, must be unique.
+                </FormDescription>
+              </div>
+              <div className="flex flex-col w-full space-y-2">
+                <FormControl>
+                  <Input
+                    type="file"
+                    accept="image/*"
+                    onChange={handleImageChange}
+                    onAbort={() => setSelectedImage(null)}
+                  />
+                </FormControl>
+                <FormMessage />
+                {selectedImage && (
+                  <img
+                    src={selectedImage}
+                    alt={"Selected product image"}
+                    className="w-full md:w-40 aspect-square object-cover shadow-md"
+                  />
+                )}
+              </div>
+            </FormItem>
+          )}
+        />
+        <div className="flex flex-col md:flex-row gap-4 justify-end mt-4 border-t pt-4 px-4">
+          <Button
+            className="w-full md:max-w-[240px] self-center  "
+            type="reset"
+            variant={"outline"}
+            onClick={onClear}
+          >
+            Clear
+          </Button>
+          <Button
+            className="w-full md:max-w-[240px] self-center  "
+            type="submit"
+          >
+            Submit
+          </Button>
+        </div>
       </form>
     </Form>
   );
--- src/lib/validations/product.ts
+++ src/lib/validations/product.ts
@@ -3,6 +3,17 @@ import { createInsertSchema } from "drizzle-zod";
 
 import { products } from "@/db/schema";
 
-const insertProductSchema = createInsertSchema(products, {});
+const insertProductSchema = createInsertSchema(products, {
+  id: z.number().int().positive(),
+  name: z
+    .string()
+    .min(1, "Name is required")
+    .max(255, "Name must be at most 255 characters long"),
+  sku: z
+    .string()
+    .min(1, "SKU is required")
+    .max(128, "SKU must be at most 128 characters long"),
+  description: z.string().min(1, "Description is required"),
+});
 
 export { insertProductSchema as CreateProduct };
