1be9508b00aa1bb85e92957d54a504570fa6d1f1
Armanda Arif <ceedadev@gmail.com>
2023-12-14 09:25:27 +0700

    Refactor product page layout and add table with CRUD actions

--- src/app/product/page.tsx
+++ src/app/product/page.tsx
@@ -1,8 +1,23 @@
-import PaginationControl from "@/components/pagination-control";
-import { Sheet } from "@/components/sheet";
+import { asc, sql } from "drizzle-orm";
+import { TrashIcon, Pencil2Icon } from "@radix-ui/react-icons";
+
 import { db } from "@/db/connect";
 import { products } from "@/db/schema";
-import { asc, sql } from "drizzle-orm";
+
+import { Sheet } from "@/components/sheet";
+import PaginationControl from "@/components/pagination-control";
+
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+import { Switch } from "@/components/ui/switch";
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table";
 
 export default async function ProductPage({
   searchParams,
@@ -36,21 +51,48 @@ export default async function ProductPage({
       <p>{JSON.stringify(allProducts)}</p>
       <p>{JSON.stringify(totalProducts)}</p>
       <p>{numberOfPages}</p>
-      <div className="flex flex-col space-y-4">
-        {allProducts.map((product) => (
-          <div
-            key={product.id}
-            className="flex flex-row justify-between items-center px-4 py-2 bg-white rounded-md shadow-md"
-          >
-            <p>{product.name}</p>
-          </div>
-        ))}
-        <PaginationControl
-          totalPages={numberOfPages}
-          hasNextPage={page < numberOfPages}
-          hasPrevPage={page > 1}
-        />
-      </div>
+      <Table>
+        <TableHeader>
+          <TableRow>
+            <TableHead>Id</TableHead>
+            <TableHead>Name</TableHead>
+            <TableHead>Price</TableHead>
+            <TableHead>Stock</TableHead>
+            <TableHead className="text-center">Active</TableHead>
+            <TableHead className="text-center">Actions</TableHead>
+          </TableRow>
+        </TableHeader>
+        <TableBody>
+          {allProducts.map((product) => (
+            <TableRow key={product.id}>
+              <TableCell>{product.id}</TableCell>
+              <TableCell className="">{product.name}</TableCell>
+              <TableCell className="w-40">
+                <Input type="number" value={Number(product.price)} />
+              </TableCell>
+              <TableCell className="w-40">
+                <Input type="number" value={Number(product.quantity)} />
+              </TableCell>
+              <TableCell className="w-12">
+                <Switch checked={product.isActive} />
+              </TableCell>
+              <TableCell className="space-x-4 w-32 justify-between">
+                <Button variant={"ghost"}>
+                  <Pencil2Icon />
+                </Button>
+                <Button variant={"ghost"}>
+                  <TrashIcon className="text-destructive" />
+                </Button>
+              </TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+      <PaginationControl
+        totalPages={numberOfPages}
+        hasNextPage={page < numberOfPages}
+        hasPrevPage={page > 1}
+      />
     </Sheet>
   );
 }
