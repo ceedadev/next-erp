d28a122f4baed2f362815daf0e27a5b04be33123
ceedadev <ceedadev@gmail.com>
2023-12-17 09:29:21 +0700

    Update findFirst query to include a where clause and handle product not found error

--- src/lib/actions/product.ts
+++ src/lib/actions/product.ts
@@ -3,13 +3,18 @@
 import { db } from "@/db";
 import { products, type Product } from "@/db/schema";
 import { productSchema } from "@/lib/validations/product";
+import { and, eq } from "drizzle-orm";
 import { revalidatePath } from "next/cache";
 import { z } from "zod";
 
 export async function updateProduct(input: z.infer<typeof productSchema>) {
-  const product = await db.query.products.findFirst({});
+  const product = await db.query.products.findFirst({
+    where: eq(products.id, input.id),
+  });
 
-  //   await db.update(products).set(input).where();
+  if (!product) {
+    throw new Error("Product not found");
+  }
 
   revalidatePath("/products");
 }
