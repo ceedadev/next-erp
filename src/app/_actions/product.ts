0b1a54b2f22f0f05ddc54f148e6b533d87391468
ceedadev <ceedadev@gmail.com>
2024-01-11 11:39:21 +0700

    product sort by sku

--- src/app/_actions/product.ts
+++ src/app/_actions/product.ts
@@ -1,6 +1,6 @@
 "use server";
 
-import { eq } from "drizzle-orm";
+import { asc, desc, eq } from "drizzle-orm";
 
 import { db } from "@/db";
 import { products } from "@/db/schema";
@@ -9,6 +9,7 @@ export async function getAllActiveProducts() {
   const data = await db
     .select()
     .from(products)
-    .where(eq(products.isActive, true));
+    .where(eq(products.isActive, true))
+    .orderBy(asc(products.sku));
   return data;
 }
